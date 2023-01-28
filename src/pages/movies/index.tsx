99973bf711525f492161441a212b846bc50c2feb
Rikard Johansson <Rikard.com@hotmail.com>
2023-01-28 23:00:40 +0100

    added movie route

--- src/pages/movies/index.tsx
+++ src/pages/movies/index.tsx
@@ -1,7 +1,14 @@
+import MovieWrapper from "@/components/MovieWrapper/MovieWrapper";
+import Navbar from "@/components/Navbar/Navbar";
 import React from "react";
 
 const Movies = () => {
-  return <div>Movies</div>;
+  return (
+    <div className='bg-lightBackground'>
+      <Navbar />
+      <MovieWrapper type='movie' sort='year' limit='20' order='-1' search='' />
+    </div>
+  );
 };
 
 export default Movies;
