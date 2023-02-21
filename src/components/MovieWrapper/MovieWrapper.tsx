b568a16f9828d65bb55df222c9bddbfc32f3fc02
Rikard Johansson <Rikard.com@hotmail.com>
2023-02-21 21:19:32 +0100

    Added Skeleton Loader

--- src/components/MovieWrapper/MovieWrapper.tsx
+++ src/components/MovieWrapper/MovieWrapper.tsx
@@ -12,6 +12,22 @@ const MovieWrapper = ({ type, sort, order, limit, genre, search }: any) => {
     error,
   } = useFetchData(type, sort, order, limit, genre, search);
 
+  if (loading) {
+    return (
+      <section className=' grid grid-cols-auto-fit-200 p-4 gap-4'>
+        {Array(50)
+          .fill("")
+          .map((_, i: number) => (
+            <div key={i} className='animate-pulse'>
+              <div className='rounded-lg bg-black aspect-[10/16]'></div>
+              <div className='h-5 my-2 rounded-lg w-full bg-black  '></div>
+              <div className='h-3 w-10 rounded-lg bg-black '></div>
+            </div>
+          ))}
+      </section>
+    );
+  }
+
   if (error) {
     return <p>Error: {error}</p>;
   }
